b4dd1839732abaf666e8244937562cca9afe068f
Asha <ashaa2@illinois.edu>
2018-04-26 14:30:14 -0500

    Add eraser files

---
+++ src/eraser.h
@@ -0,0 +1,24 @@
+//
+// Created by Asha on 4/24/2018.
+//
+
+#ifndef FINAL_PROJECT_ASHAAGRAWAL07_ERASER_H
+#define FINAL_PROJECT_ASHAAGRAWAL07_ERASER_H
+
+#include "tool.h"
+using namespace std;
+
+class Eraser : public Tool {
+public:
+    /**
+     * this is the alpha value of the background color, which will serve as the upperbound for the alpha values of the eraser
+     */
+    int bg_a_val;
+
+public:
+    //constructor
+    //inputs will be the rbga straight from the background color
+    Eraser(int r, int g, int b, int a, int w) : Tool(r, g, b, a, w), bg_a_val(a) {}
+
+};
+#endif //FINAL_PROJECT_ASHAAGRAWAL07_ERASER_H
